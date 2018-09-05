c68779c7709a9dafe45a8342c73827ebd589eec8
Huy Tran <huytp.uit@gmail.com>
2018-09-05 18:09:50 +0700

    Add search function on histories

--- app/controllers/operator/histories_controller.rb
+++ app/controllers/operator/histories_controller.rb
@@ -1,5 +1,14 @@
 class Operator::HistoriesController < ApplicationController
   def index
-    @histories = History.all.page(params[:page])
+    if params[:key] && params[:key] != ""
+      kyc = KycAddress.where("email like :k OR address like :k OR mis_address like :k",k: "%#{params[:key]}%").first
+      if kyc.present?
+        @histories = kyc.histories.page(params[:page])
+      else
+        @histories = History.none.page(params[:page])
+      end
+    else
+     @histories = History.all.page(params[:page])
+   end
   end
 end
