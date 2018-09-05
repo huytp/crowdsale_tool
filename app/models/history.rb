0a21b3cd2017375a49360d0570068edcc3a6fd01
Huy Tran <huytp.uit@gmail.com>
2018-09-05 15:18:21 +0700

    Show detail transaction info at history

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,4 +1,5 @@
 class History < ApplicationRecord
   belongs_to :fixed_campaign,  optional: true
   belongs_to :kyc_address
+  belongs_to :tran, foreign_key: :transaction_id, class_name: "Transaction"
 end
